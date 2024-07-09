0266864727451d628e7a3770e6333fd3fdf08c31
Vladulento <100429327@alumnos.uc3m.es>
2024-07-09 14:42:57 +0200

    comentar código y poner todo en /strings así como ordenarlo

--- app/src/main/java/com/uc3m/InmunoApp/Account/UserFragment.java
+++ app/src/main/java/com/uc3m/InmunoApp/Account/UserFragment.java
@@ -18,6 +18,7 @@ import androidx.fragment.app.Fragment;
 
 import android.text.InputType;
 
+import android.text.TextUtils;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
@@ -45,10 +46,12 @@ public class UserFragment extends Fragment {
         Button logoutButton = view.findViewById(R.id.logoutButton);
         Button passwordButton = view.findViewById(R.id.changePasswrdButton);
         Button deleteButton = view.findViewById(R.id.deleteButton);
+        Button emailButton = view.findViewById(R.id.changeEmail);
 
         logoutButton.setOnClickListener(v -> logoutUser());
         deleteButton.setOnClickListener(v -> deleteUser());
         passwordButton.setOnClickListener(v -> changePassword());
+        emailButton.setOnClickListener(v -> changeEmail());
 
         //Base de datos en tiempo real en vez de SharedPreferences
 
@@ -192,6 +195,81 @@ public class UserFragment extends Fragment {
         });
 
         builder.setNegativeButton("Cancelar", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void changeEmail(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            Toast.makeText(getActivity(), "Usuario no registrado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Cambio de correo electrónico");
+
+        // Crear el layout para el diálogo
+        LinearLayout mainLayout = new LinearLayout(getActivity());
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+        mainLayout.setPadding(50, 40, 50, 10); // Añadir padding para espacio extra
+
+        // Mostrar el email actual
+        TextView currentEmailView = new TextView(getActivity());
+        currentEmailView.setText(String.format(getString(R.string.current_email), user.getEmail()));
+        mainLayout.addView(currentEmailView);
+
+        // Añadir un espacio entre el texto y el primer EditText
+        Space space = new Space(getActivity());
+        LinearLayout.LayoutParams spaceParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, 20);
+        space.setLayoutParams(spaceParams);
+        mainLayout.addView(space);
+
+        // Crear el primer EditText para el nuevo email
+        final EditText newEmailInput = new EditText(getActivity());
+        newEmailInput.setHint("Introduce tu nuevo email");
+        newEmailInput.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        newEmailInput.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        mainLayout.addView(newEmailInput);
+
+        // Crear el segundo EditText para confirmar el nuevo email
+        final EditText confirmEmailInput = new EditText(getActivity());
+        confirmEmailInput.setHint("Confirma tu nuevo email");
+        confirmEmailInput.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        confirmEmailInput.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        mainLayout.addView(confirmEmailInput);
+
+        builder.setView(mainLayout);
+
+        builder.setPositiveButton("Change", (dialogInterface, i) -> {
+            String newEmail = newEmailInput.getText().toString().trim();
+            String confirmEmail = confirmEmailInput.getText().toString().trim();
+
+            if (!TextUtils.isEmpty(newEmail) && android.util.Patterns.EMAIL_ADDRESS.matcher(newEmail).matches() && newEmail.equals(confirmEmail)) {
+                FirebaseUser user1 = FirebaseAuth.getInstance().getCurrentUser();
+                if (user1 != null) {
+                    user1.verifyBeforeUpdateEmail(newEmail)
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getActivity(), String.format(getString(R.string.verification_email_sent), newEmail), Toast.LENGTH_SHORT).show();
+                                    dialogInterface.dismiss();  // Cerrar el diálogo actual
+                                    // Mostrar el diálogo nuevamente para reflejar el cambio
+                                    changeEmail();
+                                } else {
+                                    Toast.makeText(getActivity(), "Cambio de correo electrónico falló", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            } else {
+                Toast.makeText(getActivity(), "Los correos electrónicos no coinciden o tienen un formato inválido", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", null);
 
         AlertDialog dialog = builder.create();
         dialog.show();
